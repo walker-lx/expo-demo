bedc5ea3b75f601686274138e58c9ffdbd26217a
张杰-长安-前端 <zhangjie2026@changan.com.cn>
2025-08-12 17:43:46 +0800

    update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Home1',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
